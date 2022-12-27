6036fd03899b55b2842c7ebbdeac224aa0d74377
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-12-27 16:28:57 -0300

    feat: include location

--- lib/pages/place_form_page.dart
+++ lib/pages/place_form_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
@@ -59,6 +60,8 @@ class _PlaceFormPageState extends State<PlaceFormPage> {
                     ),
                     const SizedBox(height: 10),
                     ImageInput(_selectedImage),
+                    const SizedBox(height: 10),
+                    const LocationInput(),
                   ],
                 ),
               ),
